a8e7100561059c655c23fb6b8f6824eb792205cf
Kuang-Yang <56757171+aurorasun27@users.noreply.github.com>
2024-03-28 20:47:06 -0400

    (1) finished data preprocessing. (2) created MDP tuples (s, a, r, s').

---
+++ data_preprocessing.py
@@ -0,0 +1,77 @@
+#import pandas as pd
+#import numpy as np
+
+# get current motion data
+class motionState:
+    def __init__(self, time_stamp_ms):
+        assert isinstance(time_stamp_ms, int)
+        self.time_stamp_ms = time_stamp_ms
+        self.agent_type = None
+        self.x = None
+        self.y = None
+        self.vx = None
+        self.vy = None
+        self.psi_rad = None
+
+    def __str__(self):
+        return "motionState: " + str(self.__dict__)
+
+    def get_dict_type_data(self):
+        return {'agent_type':self.agent_type, 'x':self.x, 'y':self.y, 'vx':self.vx, 'vy':self.vy, 'psi_rad':self.psi_rad}
+
+# each unique_id (= case_id * 100 + track_id) represents either a car, pedestrian, or bike
+class uniqueTrack:
+    def __init__(self, unique_id):
+        self.case_id = None
+        self.track_id = None
+        self.unique_id = unique_id
+        self.agent_type = None
+        self.length = None
+        self.width = None
+        self.time_stamp_ms_first = None
+        self.time_stamp_ms_last = None
+        self.motionState = {}
+
+    def __str__(self):
+        string = f"uniqueTrack: case_id={self.case_id}, track_id={self.track_id}, \
+                unique_id={self.unique_id}, agent_type={self.agent_type}, \
+                length={self.length}, width={self.width}" 
+        for key, value in sorted(self.motionState.items()):
+            string += "\n    " + str(key) + ": " + str(value)
+        return string
+
+# get each uniqueTrack data
+def read_uniqueTracks(df):
+    uniqueTracks = {}
+
+    for index, row in df.iterrows():
+        unique_id = row['unique_id']
+        if unique_id not in uniqueTracks:
+            track = uniqueTrack(unique_id)
+            track.case_id = row['case_id']
+            track.track_id = row['track_id']
+            track.agent_type = row['agent_type']
+            track.length = row['length']
+            track.width = row['width']
+            track.motionState = {}
+            uniqueTracks[unique_id] = track
+        else:
+            track = uniqueTracks[unique_id]
+
+        timestamp = row['timestamp_ms']
+        motion_state = {
+            'agent_type': row['agent_type'],
+            'x': row['x'], 
+            'y': row['y'], 
+            'vx': row['vx'], 
+            'vy': row['vy'], 
+            'psi_rad': row['psi_rad']
+        }
+
+        track.motionState[timestamp] = motion_state
+
+    return uniqueTracks
+
+
+
+
---
+++ mdp.py
@@ -0,0 +1,54 @@
+##### Note: Markov Decision Process
+# S: the set of states
+# A: the set of actions
+# P: the dynamic transition model
+# R: the reward function
+# r: discount factor
+
+
+def get_other_agents_unique_id(unique_id, current_time, uniqueTracks):
+    other_agents_id_list = []
+    x  = uniqueTracks[unique_id].motionState[current_time]['x']
+    y  = uniqueTracks[unique_id].motionState[current_time]['y']
+    distance = {}
+
+    case_id = unique_id // 100
+    for agent_id in uniqueTracks:
+        if agent_id // 100 == case_id and agent_id != unique_id:
+            # if uniqueTracks[agent_id].agent_type == "car":
+            value = uniqueTracks[agent_id].motionState.get(current_time, "Not Found")
+            if value != "Not Found":
+                other_agents_id_list.append(agent_id)
+                x_prim = uniqueTracks[agent_id].motionState[current_time]['x']
+                y_prim = uniqueTracks[agent_id].motionState[current_time]['y']
+                distance[agent_id] = (y_prim - y) ** 2 + (x_prim - x) ** 2
+    sorted_three_agetns = [item[0] for item in sorted(distance.items(), key=lambda item: item[1])][:3]
+    return other_agents_id_list, sorted_three_agetns
+
+
+def MDP_tuple(ego_id, interactive_agents, current_time, uniqueTracks):
+
+    ### 1. state
+    # ego vehicle id + interactive ids + ego info.
+    egoMotionState = uniqueTracks[ego_id].motionState
+    s = [ego_id] + interactive_agents + list(egoMotionState[current_time].values())
+    # add interactive vehicles info.
+    for agent_id in interactive_agents:
+        s += list(uniqueTracks[agent_id].motionState[current_time].values())
+    
+    ### 2. action -> velocity in the next timestep
+    next_time = current_time + 100
+    a = [egoMotionState[next_time]['vx'], egoMotionState[next_time]['vy']]
+
+    ### 3. reward
+    r = 0
+
+    ### 4. next state
+    # get next interactive agents
+    _, interactive_agents_next = get_other_agents_unique_id(ego_id, next_time, uniqueTracks)
+    # repeat step 1. to get next state
+    s_next = [ego_id] + interactive_agents_next + list(egoMotionState[next_time].values())
+    for agent_id in interactive_agents_next:
+        s_next += list(uniqueTracks[agent_id].motionState[next_time].values())
+
+    return (s, a, r, s_next)
